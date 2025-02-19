4af67533fa378e2a4fefb6ad7565cf8d3e62b925
Yahya Alhinai <37601653+yhinai@users.noreply.github.com>
2025-02-18 23:55:57 -0800

    Update hlos.sh

--- hlos.sh
+++ hlos.sh
@@ -8,9 +8,17 @@
 STANDARD_OEM_DIR="${PWD}/matrix-la-1-0_ap_standard_oem"
 FIX_DUPLICATE_SCRIPT="${PWD}/fix_duplicate_modules.py"
 
-KERNEL_DIR="${STANDARD_OEM_DIR}/BUILD/KERNEL.PLATFORM.3.0.r13"
-QSSI_DIR="${STANDARD_OEM_DIR}/BUILD/LA.QSSI.14.0.r1"
-VENDOR_DIR="${STANDARD_OEM_DIR}/BUILD/LA.VENDOR.14.3.3.r1"
+KERNEL_DIR="${STANDARD_OEM_DIR}/KERNEL.PLATFORM.3.0.r13"
+QSSI_DIR="${STANDARD_OEM_DIR}/LA.QSSI.14.0.r1"
+VENDOR_DIR="${STANDARD_OEM_DIR}/LA.VENDOR.14.3.3.r1"
+
+KERNEL_WROKSPACE="${STANDARD_OEM_DIR}/BUILD/KERNEL_WORKSPACE"
+QSSI_WROKSPACE="${STANDARD_OEM_DIR}/BUILD/QSSI_WORKSPACE"
+VENDOR_WROKSPACE="${STANDARD_OEM_DIR}/BUILD/VENDOR_WORKSPACE"
+
+mkdir -p "${KERNEL_WROKSPACE}"
+mkdir -p "${QSSI_WROKSPACE}"
+mkdir -p "${VENDOR_WROKSPACE}"
 
 SYNC_SCRIPT="${STANDARD_OEM_DIR}/LA.VENDOR.14.3.3.r1/LINUX/android/sync_snap_v2.sh"
 
@@ -40,17 +48,19 @@ chmod +x "$SYNC_SCRIPT"
 
 # ---------[KERNEL.PLATFORM]--------- #
 
-cd "${KERNEL_DIR}"
+cd "${KERNEL_WROKSPACE}"
+
 # Sync Kernel
 "$SYNC_SCRIPT" \
     --jobs="$(nproc)" \
-    --workspace_path="${KERNEL_DIR}" \
+    --workspace_path="${KERNEL_WROKSPACE}" \
     --snap_release="${KERNEL_DIR}/kernel_platform/snap_release.xml" \
     --tree_type=KERNEL.PLATFORM.3.0.r13 \
     --prop_opt=chipcode \
     --repo_url=git@git.codelinaro.org:/clo/tools/repo.git \
     --repo_branch=aosp-new/stable \
     --nhprop_chipcode_path="${STANDARD_OEM_DIR}"
+
 
 mkdir -p vendor/qcom/defs
 
@@ -70,10 +80,10 @@ export PATH=$JAVA_HOME/bin:$PATH
 git config --global --unset url.git@git.codelinaro.org:.insteadOf
 
 # Sync QSSI
-cd "${QSSI_DIR}"
+cd "${QSSI_WROKSPACE}"
 "$SYNC_SCRIPT" \
     --jobs="$(nproc)" \
-    --workspace_path="${QSSI_DIR}" \
+    --workspace_path="${QSSI_WROKSPACE}" \
     --snap_release="${QSSI_DIR}/LINUX/android/snap_release.xml" \
     --tree_type=LA.QSSI.14.0.r1 \
     --prop_opt=chipcode \
@@ -81,12 +91,11 @@ cd "${QSSI_DIR}"
     --repo_branch=aosp-new/stable \
     --nhprop_chipcode_path="${STANDARD_OEM_DIR}"
 
-# Run fix_duplicate_modules script
-python3 FIX_DUPLICATE_SCRIPT
+rm -rf out
+make clean
 
 source build/envsetup.sh
 lunch qssi_xrM-userdebug
-make clean
 bash build.sh -j128 dist --qssi_only EXPERIMENTAL_USE_OPENJDK9=1.8
 
 # Restore git config
@@ -95,12 +104,12 @@ git config --global url.git@git.codelinaro.org:.insteadOf https://git.codelinaro
 
 # ----------[VENDOR]---------- #
 
-cd "${VENDOR_DIR}"
+cd "${VENDOR_WROKSPACE}"
 
 # Sync Vendor
 "$SYNC_SCRIPT" \
     --jobs="$(nproc)" \
-    --workspace_path="${VENDOR_DIR}" \
+    --workspace_path="${VENDOR_WROKSPACE}" \
     --snap_release="${VENDOR_DIR}/LINUX/android/snap_release.xml" \
     --tree_type=vendor_techpack \
     --prop_opt=chipcode \
@@ -108,18 +117,20 @@ cd "${VENDOR_DIR}"
     --repo_branch=aosp-new/stable \
     --nhprop_chipcode_path="${STANDARD_OEM_DIR}"
 
-# Copy required files
-cp -prv "${QSSI_DIR}"/* "${VENDOR_DIR}/"
-rm -rf "${VENDOR_DIR}"/out
+# # Copy required files
+# cp -r "${QSSI_WROKSPACE}"/* "${VENDOR_WROKSPACE}/"
+# rm -rf "${VENDOR_WROKSPACE}"/out
 
-cp -prv "${KERNEL_DIR}"/kernel_platform/ "${VENDOR_DIR}/"
-rm -rf "${VENDOR_DIR}"/kernel_platform/out
+# cp -r "${KERNEL_WROKSPACE}"/kernel_platform/ "${VENDOR_WROKSPACE}/"
+# rm -rf "${VENDOR_WROKSPACE}"/kernel_platform/out
 
-mkdir -p "${VENDOR_DIR}"/out/
-cp -r "${KERNEL_DIR}"/kernel_platform/out/* "${VENDOR_DIR}"/out/
+# mkdir -p "${VENDOR_WROKSPACE}"/out/
+# cp -r "${KERNEL_WROKSPACE}"/kernel_platform/out/* "${VENDOR_WROKSPACE}"/out/
 
-mkdir -p "${VENDOR_DIR}"/vendor/qcom/proprietary/prebuilt_HY11/
-cp -r "${VENDOR_DIR}"/LINUX/android/vendor/qcom/proprietary/prebuilt_HY11/Android.mk "${VENDOR_DIR}"/vendor/qcom/proprietary/prebuilt_HY11/Android.mk
+# mkdir -p "${VENDOR_WROKSPACE}"/vendor/qcom/proprietary/prebuilt_HY11/
+# cp -r "${VENDOR_WROKSPACE}"/LINUX/android/vendor/qcom/proprietary/prebuilt_HY11/Android.mk "${VENDOR_WROKSPACE}"/vendor/qcom/proprietary/prebuilt_HY11/Android.mk
+# rm -rf out
+# make clean
 
 # Build Vendor
 source build/envsetup.sh
@@ -131,11 +142,13 @@ bash build.sh -j128 dist --target_only
 # =======================================
 # Generate super.img
 # =======================================
+cd "${VENDOR_WROKSPACE}"
+
 python vendor/qcom/opensource/core-utils/build/build_image_standalone.py \
     --image super \
-    --qssi_build_path "${QSSI_DIR}" \
-    --target_build_path "${VENDOR_DIR}" \
-    --merged_build_path "${VENDOR_DIR}" \
+    --qssi_build_path "${QSSI_WROKSPACE}" \
+    --target_build_path "${VENDOR_WROKSPACE}" \
+    --merged_build_path "${VENDOR_WROKSPACE}" \
     --target_lunch niobe \
     --output_ota \
     --skip_qiifa
